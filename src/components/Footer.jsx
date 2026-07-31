699c0bdcdcae110928a1b19a36209195557e91a1
Muhammad-Ahmed19 <ay8880625@gmail.com>
2026-07-31 13:26:02 +0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="mt-8 border-t border-gray-700 pt-8 text-center text-sm text-gray-400">
-                    Food Mart - A frontend project &copy; {new Date().getFullYear()}
+                    Food Mart - A frontend only.  &copy; {new Date().getFullYear()}
                 </div>
             </div>
         </footer>
